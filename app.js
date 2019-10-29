5aa5ea4d78fa796c1a5ae9512df97975fd5755a4
肖娇静 <840521409@qq.com>
2019-10-29 10:21:12 +0800

    修改端口号

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const tagRoute = require('./route/tag.js')
 
 const app = express()
 
-// app.all('*', (req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-//     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("Access-Control-Allow-Credentials", true); // 可以带cookies
-//     if (req.method == 'OPTIONS') {
-//         res.sendStatus(200);
-//     } else {
-//         next();
-//     }
-// })
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true); // 可以带cookies
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+})
 
 // parse application/x-www-form-urlencodeddist
 app.use(bodyParser.urlencoded({extended: false}))
@@ -32,6 +32,6 @@ app.use(express.static(path.resolve(__dirname, 'dist')))
 app.use(articleRoute)
 app.use(tagRoute)
 
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('服务器开启了...')
 })
